706764987abcdf6c4b5392fa675df385d55e2606
Marvelous Akporowho <solomonmarvel@hotmail.com>
2022-08-25 12:21:10 +0100

    added assignment files

---
+++ src/data.js
@@ -0,0 +1,39 @@
+exports.data = [
+  {
+    id: 1,
+    status: "active",
+    id: "nodejs-masterclass",
+    header_image:
+      "https://images.unsplash.com/photo-1581093805071-a04e696db334?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=50",
+    title: "Building Applications with NodeJS from Zero to One",
+    description: `Node.js, like many other technologies, has its supporters and detractors. However, there is no denying that it is widely used by some megastar platforms, such as Uber, LinkedIn, and PayPal—making it a major player in full stack development.`,
+    syllabus_link: "https://paystack.com",
+    prerequisites: [
+      "You should have a basic understanding of Javascript",
+      "Prepare your learning materials (laptops, good internet connection)",
+    ],
+    outcomes: [
+      "Understand NodeJS & the V8 Engine",
+      "Learn the Global Variables in NodeJS",
+      "Learn Core NodeJS Modules",
+      "Express JS Workshop (Learn to build web applications using ExpressJS)",
+      "Learn to manage multiple node versions with NVM (Node Version Manager)",
+      "NodeJS Program Execution, Management & Monitoring using PM2 (Offline & Cloud)",
+      "Introduction to working with Node & SQLite",
+      "Introduction to working with Node & MongoDB",
+    ],
+    duration: "3 weeks",
+    video:
+      "https://res.cloudinary.com/hayche-network/video/upload/v1646928444/website/videos/19_kbugbr.mp4",
+    intro_video_cover:
+      "https://res.cloudinary.com/hayche-network/image/upload/v1646928478/website/videos/19_bnu7mc.jpg",
+    payment_link: "https://paystack.com/pay/aywznkh13p",
+    instructor: {
+      name: "Marvelous .S. Akporowho",
+      about:
+        "Marvelous is the founder and CEO of @topuniverse, as well as a Solutions Architect at @boltcliq, and has over 12 years of software engineering experience. He is an engineering mentor and technical leader who is passionate about creating solutions using modern architectural patterns in a variety of industries.",
+      image: "https://solomonmarvel.com/img/avatar.0d9b2d8f.png",
+      url: "https://solomonmarvel.com",
+    },
+  },
+];
