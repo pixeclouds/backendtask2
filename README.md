fcb45a127e07db85714d92b6e67b61bfa7e62b66
Littledropbootcamp <littledropedu@gmail.com>
2022-08-30 14:40:12 -1200

    commit solution

--- README.md
+++ README.md
@@ -2,12 +2,12 @@
 
 Express JS Task
 
-1. Cone the project from the Github repo 
+1. Clone the project from the Github repo 
 
 [https://github.com/top-universe/backendtask2](https://github.com/top-universe/backendtask2)
 
 1. Create a simple crud app to perform: GET, PUT, DELETE AND POST requests using the data.js array as an array database.
-2. Attach your postman collection file o the project 
+2. Attach your postman collection file to the project 
 3. Ensure your codebase is properly commented
 4. Upload your codebase to Github
 
--- src/app.js
+++ src/app.js
@@ -1,1 +1,66 @@
 // write your code here
+const express = require("express")
+const port = 3000
+const app = express()
+const { database } = require("./data")
+
+app.use(express.json())
+
+
+app.get('/', (req, res) => {
+
+    let courses =  []
+    //get all open courses
+    database.forEach(course => {
+        if(course.status == "open"){
+            courses.push(course)
+        } 
+    });
+    res.send(courses)
+});
+
+
+app.post('/', (req, res) => {
+
+    let newCourse = req.body;
+    database.push(newCourse)
+    res.json({
+        newCourse, 
+        success: "New course added successfully"
+    })
+});
+
+app.put('/:id', (req, res) => {
+
+    let id = req.params.id
+    let status = req.body.status
+    //update course enrollment status
+    database.forEach(course => {
+        if(course.id == id){
+            let index = database.indexOf(course)
+            course.status = status;
+            res.json({
+                course,
+                message: `Course with id ${index + 1} updated successfully`
+            })
+        }
+    })
+});
+
+app.delete('/:id', (req, res) => {
+
+    let id = req.params.id
+     database.forEach(course => {
+        if(course.id == id){
+            let index = database.indexOf(course)
+            database.splice(index, 1)    
+            res.json(`Course with id ${index + 1} deleted successfully`)
+        }
+    });
+});
+
+
+
+app.listen(port, () => {
+    console.log(`Crud app listening on port ${port}`)
+});
